958ab49dbf928c1e1481bf9d4e0559a2e2bf2f46
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-12-04 20:04:02 +0100

    Early implementation.

--- README.md
+++ README.md
@@ -2,10 +2,18 @@
 
 Conway's Game of Life is an evolutionary game that requires no user input other than the initial state of the board. The game consists of a grid of cells that can either be alive or dead and follow a few rules to determine their next generation. These rules are:
 
-- An alive cell with fewer than two neighbors will be dead in the next generation.
-- An alive cell with two or three alive neighbors will remain alive in the next generation.
-- An alive cell with more than three neighbors will be dead in the next generation.
-- A dead cell with exactly three alive neighbors will come back to life in the next generation.
+- Any live cell with fewer than two live neighbours dies, as if by underpopulation. 
+- Any live cell with two or three live neighbours lives on to the next generation.
+- Any live cell with more than three live neighbours dies, as if by overpopulation.
+- Any **dead** cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+These rules, which compare the behavior of the automaton to real life, can be condensed into the following:
+
+- Any live cell with two or three neighbors survives.
+- Any dead cell with three live neighbors becomes a live cell.
+- All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+The initial pattern constitutes the seed of the system. The first generation is created by applying the above rules simultaneously to every cell in the seed; births and deaths occur simultaneously, and the discrete moment at which this happens is sometimes called a tick. Each generation is a pure function of the preceding one. The rules continue to be applied repeatedly to create further generations.
 
 This repository contains a solution that:
 
--- src/main/kotlin/GameOfLife.kt
+++ src/main/kotlin/GameOfLife.kt
@@ -1,3 +1,79 @@
 import arrow.core.*
+import arrow.core.extensions.list.semigroupal.times
+import arrow.mtl.State
+import arrow.mtl.run
+import arrow.typeclasses.internal.IdBimonad
 
-val gameOfLife: Nothing = TODO()
+private data class Cell(val isAlive: Boolean)
+
+private fun Cell.isDead() = !isAlive
+
+private typealias Universe = List<List<Cell>>
+
+private val Universe.sizeX: Int get() = size
+
+private val Universe.sizeY: Int get() = get(0).size
+
+private data class Position(val x: Int, val y: Int)
+
+private fun Universe.cellPosition(cell: Cell): Option<Position> =
+    this.flatten().indexOf(cell).let { index ->
+        if (index == -1) {
+            None
+        } else {
+            val x = index / sizeY
+            val y = index / sizeX
+            Some(Position(x, y))
+        }
+    }
+
+// Neighbors
+
+private fun Cell.neighbours(universe: Universe): List<Cell> {
+    val deltas = listOf(-1, 0, 1) * listOf(-1, 0, 1) // ListK cartesian product to get all combinations
+    return deltas
+        .map { Position(it.a, it.b) }
+        .filter { pos ->
+            pos.x in 0 until universe.sizeX &&
+                    pos.y in 0 until universe.sizeY &&
+                    pos.some() != universe.cellPosition(this)
+        }
+        .map { universe[it.x][it.y] }
+}
+
+private fun Cell.aliveNeighbours(universe: Universe): List<Cell> = neighbours(universe).filter { it.isAlive }
+
+private fun Universe.tick(): Tuple2<Universe, Unit> {
+    val newGeneration = this.map { column ->
+        column.map { cell ->
+            val aliveNeighbors = cell.aliveNeighbours(this).size
+            when {
+                aliveNeighbors < 2 -> Cell(isAlive = false)
+                aliveNeighbors > 3 -> Cell(isAlive = false)
+                aliveNeighbors == 3 && cell.isDead() -> Cell(isAlive = true)
+                else -> Cell(isAlive = true)
+            }
+        }.k()
+    }.k()
+    return Tuple2(newGeneration, Unit)
+}
+
+private fun gameOfLife(): State<Universe, Unit> =
+    State { universe: Universe ->
+        println(universe)
+        universe.tick()
+    }.flatMap(IdBimonad) {
+        Thread.sleep(1000)
+        gameOfLife()
+    }
+
+fun main() {
+    // State provides a convenient run method to run it using an initial state.
+    gameOfLife().run(initialSeed())
+}
+
+private fun initialSeed(): List<List<Cell>> =
+    listOf(
+        listOf(Cell(false), Cell(true)),
+        listOf(Cell(false), Cell(false))
+    )
