a4f5c7255d8fbaacec972c9983afdd5ad9d77f44
Jorge Castillo <jorge.castillo.prz@gmail.com>
2019-12-09 17:13:50 +0100

    Remove unused cell generator.

--- src/test/kotlin/Generators.kt
+++ src/test/kotlin/Generators.kt
@@ -1,15 +1,5 @@
 import io.kotlintest.properties.Gen
 
-class CellGen : Gen<Cell> {
-    override fun constants(): Iterable<Cell> = emptyList()
-
-    override fun random(): Sequence<Cell> =
-        generateSequence {
-            val random = Gen.bool().random().first()
-            if (random) Cell.Alive() else Cell.Dead()
-        }
-}
-
 class UniverseGen(private val center: Cell, private val aliveNeighbors: Int) : Gen<Universe> {
 
     override fun constants(): Iterable<Universe> = emptyList()
